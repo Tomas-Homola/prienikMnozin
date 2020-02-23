14d21285406bce8ad245df5431b699a454d7b4f5
root <root@localhost>
2020-02-23 20:09:23 +0100

    prve veci

---
+++ prienikMnozin.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main()
+{
+	int pole1[] = {1, 2, 3, 4, 5, 6}, pole2[] = {2, 4, 6};
+	int i, j;
+	
+	for (i = 0; i < (sizeof(pole1))/4; i++)
+	{
+		for (j = 0; j < (sizeof(pole2))/4; j++)
+		{
+			
+		}
+	}
+}
