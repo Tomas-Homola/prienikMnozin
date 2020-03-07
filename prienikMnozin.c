eca8758a3fb4cb4ded14655fea9f4ddc36763650
Tomas Homola <homola.tom6@gmail.com>
2020-03-07 17:45:36 +0100

    hladanie prvkov prieniku - asi funkcne

--- prienikMnozin.c
+++ prienikMnozin.c
@@ -2,14 +2,31 @@
 
 main()
 {
-	int pole1[] = {1, 2, 3, 4, 5, 6}, pole2[] = {2, 4, 6};
-	int i, j;
+	int pole1[] = {1, 2, 3, 4, 5, 6}, pole2[] = {2, 4, 6}, poleP[] = {};
+	int i, j, rovne, k;
+	
+	k = 0;
 	
 	for (i = 0; i < (sizeof(pole1))/4; i++)
 	{
+		rovne = 0;
 		for (j = 0; j < (sizeof(pole2))/4; j++)
 		{
-			
+			if (pole1[i] == pole2[j])
+			{
+				rovne++;
+			}						
 		}
+		if (rovne > 0)
+			{
+				poleP[k] = pole1[i];
+				//printf("%d\n", poleP[k]);
+				k++;
+			}
+	}
+	
+	for (i = 0; i < k; i++)
+	{
+		printf("%d\n", poleP[i]);
 	}
 }
